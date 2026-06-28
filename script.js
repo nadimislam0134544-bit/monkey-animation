8ef0a133c4d5753fef8fe0cd39e7459bd1d19dbc
nadimislam0134544-bit <nadimislam0134544@gmail.com>
2026-06-28 09:43:55 +0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+
+    const slide = document.querySelector(".slide");
+
+    slide.style.animation = "none";
+    slide.offsetHeight;
+
+    slide.style.animation = "slideDown 2s ease forwards";
+
+},3000);
